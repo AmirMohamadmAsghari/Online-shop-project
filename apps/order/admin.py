9a2efdfcf550dcb9d308f3204560f1e7c7a63e39
Amir <amirm2.asghari0@gmail.com>
2024-04-18 22:01:56 +0330

    Make file admin.py settings,order app

--- apps/order/admin.py
+++ apps/order/admin.py
@@ -1,3 +1,38 @@
 from django.contrib import admin
+from .models import Order, OrderItem, CodeDiscount, Payment, Address
 
-# Register your models here.
+
+class OrderItemInline(admin.TabularInline):
+    model = OrderItem
+    extra = 1
+
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('id', 'customer', 'total_amount', 'order_date', 'status')
+    search_fields = ('customer__email', 'order_date')
+    list_filter = ('status', 'order_date')
+    inlines = [OrderItemInline]
+
+
+class CodeDiscountAdmin(admin.ModelAdmin):
+    list_display = ('id', 'amount', 'code', 'is_deleted')
+    search_fields = ('code',)
+    list_filter = ('is_deleted',)
+
+
+class PaymentAdmin(admin.ModelAdmin):
+    list_display = ('id', 'order', 'payment_date', 'payment_type', 'transaction_id')
+    search_fields = ('order__id', 'payment_date')
+    list_filter = ('payment_date', 'payment_type')
+
+
+class AddressAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'name', 'postal_code', 'city', 'province', 'is_deleted')
+    search_fields = ('user__email', 'name', 'postal_code')
+    list_filter = ('city', 'province', 'is_deleted')
+
+
+admin.site.register(Order, OrderAdmin)
+admin.site.register(CodeDiscount, CodeDiscountAdmin)
+admin.site.register(Payment, PaymentAdmin)
+admin.site.register(Address, AddressAdmin)
