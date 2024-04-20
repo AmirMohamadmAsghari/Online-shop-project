ef3365a3bf143cdfc923ce2aad587af783c34b11
Amir <amirm2.asghari0@gmail.com>
2024-04-20 21:23:31 +0330

    Transferring the address model to the app user and changing the relationship type of some fields

--- apps/order/models.py
+++ apps/order/models.py
@@ -1,17 +1,17 @@
 from django.db import models
 from apps.user.models import CustomUser
 from apps.product.models import Product
+from apps.user.models import Address
 # Create your models here
 
 
 class Order(models.Model):
     customer = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='CustomerOrder')
     total_amount = models.DecimalField(max_digits=10, decimal_places=2)
-    address = models.OneToOneField('Address', on_delete=models.CASCADE, related_name='AddressesOrder')
+    address = models.ForeignKey(Address, on_delete=models.CASCADE, related_name='AddressesOrder')
     order_date = models.DateTimeField(auto_now_add=True)
-    discount_code = models.OneToOneField('CodeDiscount', on_delete=models.CASCADE, related_name='DiscountOrder', null=True, blank=True)
+    discount_code = models.ForeignKey('CodeDiscount', on_delete=models.CASCADE, related_name='DiscountOrder', null=True, blank=True)
     status = models.BooleanField(default=False)
-    session_id = models.IntegerField()
     is_deleted = models.BooleanField(default=False)
 
 
@@ -26,6 +26,7 @@ class OrderItem(models.Model):
 class CodeDiscount(models.Model):
     amount = models.IntegerField(default=0)
     code = models.IntegerField()
+    is_active = models.BooleanField(default=False)
     is_deleted = models.BooleanField(default=False)
 
 
@@ -36,12 +37,3 @@ class Payment(models.Model):
     transaction_id = models.IntegerField()
 
 
-class Address(models.Model):
-    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='CustomerAddress')
-    name = models.CharField(max_length=255)
-    detail_address = models.TextField(max_length=300)
-    postal_code = models.IntegerField()
-    description = models.TextField(max_length=355)
-    city = models.CharField(max_length=255)
-    province = models.CharField(max_length=255)
-    is_deleted = models.BooleanField(default=False)
