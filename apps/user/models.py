02cfd0651d0b8229735a9e3e65d074f020f05cb3
Amir <amirm2.asghari0@gmail.com>
2024-04-20 21:37:16 +0330

    Transferring the address model to the app user and changing the relationship type of some fields and delete role models and permissions

--- apps/user/models.py
+++ apps/user/models.py
@@ -17,7 +17,7 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
     is_staff = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
     date_joined = models.DateTimeField(auto_now=True)
-    role = models.ForeignKey('UserRole', on_delete=models.CASCADE, null=True, related_name='users')
+    # role = models.ForeignKey('UserRole', on_delete=models.CASCADE, null=True, related_name='users')
 
     objects = CustomUserManager()
 
@@ -30,16 +30,27 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
     groups = models.ManyToManyField(Group, verbose_name=_('groups'), blank=True, related_name='customuser_groups')
 
 
-class UserRole(models.Model):
+# class UserRole(models.Model):
+#     name = models.CharField(max_length=255)
+#
+#     def __str__(self):
+#         return self.name
+
+
+# class CustomPermission(models.Model):
+#     action = models.CharField(max_length=255)
+#     role = models.ForeignKey(UserRole, on_delete=models.CASCADE, related_name='permissions')
+#
+#     def __str__(self):
+#         return f"{self.action} - {self.role}"
+
+
+class Address(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='CustomerAddress')
     name = models.CharField(max_length=255)
-
-    def __str__(self):
-        return self.name
-
-
-class CustomPermission(models.Model):
-    action = models.CharField(max_length=255)
-    role = models.ForeignKey(UserRole, on_delete=models.CASCADE, related_name='permissions')
-
-    def __str__(self):
-        return f"{self.action} - {self.role}"
+    detail_address = models.TextField(max_length=300)
+    postal_code = models.IntegerField()
+    description = models.TextField(max_length=355)
+    city = models.CharField(max_length=255)
+    province = models.CharField(max_length=255)
+    is_deleted = models.BooleanField(default=False)
