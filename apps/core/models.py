bafd13c70ac6c4ba8959d49cf8eea1a03390a978
Amir <amirm2.asghari0@gmail.com>
2024-04-27 20:11:12 +0330

    Building app core models

--- apps/core/models.py
+++ apps/core/models.py
@@ -1,3 +1,29 @@
 from django.db import models
-
+from apps.core.managers import LogicalManager
 # Create your models here.
+
+
+class TimeStampedMixin(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class LogicalMixin(models.Model):
+    is_active = models.BooleanField(default=True)
+    is_deleted = models.BooleanField(default=False)
+
+    objects = LogicalManager()
+
+    def delete(self, using=None, keep_parents=False):
+        self.is_deleted = True
+        self.save(update_fields=['is_deleted'])
+
+    def deactivate(self):
+        self.is_active = False
+        self.save(update_fields=['is_active'])
+
+    class Meta:
+        abstract = True
