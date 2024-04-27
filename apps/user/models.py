d491961ab2e62602c6ff962c61cfaffd1baad190
Amir <amirm2.asghari0@gmail.com>
2024-04-27 20:13:13 +0330

    Updating user app models Add meta class to models

--- apps/user/models.py
+++ apps/user/models.py
@@ -1,25 +1,27 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, Group
 from .manager import CustomUserManager
+from django.core.validators import RegexValidator
 from django.utils.translation import gettext_lazy as _
-
+from apps.core.models import TimeStampedMixin, LogicalMixin
 # Create your models her
 
 
-class CustomUser(AbstractBaseUser, PermissionsMixin):
+class CustomUser(AbstractBaseUser, PermissionsMixin, LogicalMixin, TimeStampedMixin):
+    username_validator = RegexValidator(
+        r"^[a-zA-Z0-9_]*$", "Only alphanumeric characters are allowed."
+    )
     email = models.EmailField(unique=True)
-    username = models.CharField(max_length=150, null=True)
-    phone = models.IntegerField(null=True)
+    username = models.CharField(unique=True,max_length=150, null=True, validators=[username_validator])
+    phone = models.IntegerField(null=True,unique=True)
     otp_code = models.IntegerField(blank=True,null=True)
     name = models.CharField(max_length=255,blank=True,null=True)
-    session_id = models.IntegerField(blank=True,null=True)
-    is_delete = models.BooleanField(default=False)
     is_staff = models.BooleanField(default=False)
-    is_active = models.BooleanField(default=True)
     date_joined = models.DateTimeField(auto_now=True)
-    # role = models.ForeignKey('UserRole', on_delete=models.CASCADE, null=True, related_name='users')
-
     objects = CustomUserManager()
+
+    class Meta:
+        unique_together = [('email', 'username')]
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username', 'password']
@@ -30,22 +32,7 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
     groups = models.ManyToManyField(Group, verbose_name=_('groups'), blank=True, related_name='customuser_groups')
 
 
-# class UserRole(models.Model):
-#     name = models.CharField(max_length=255)
-#
-#     def __str__(self):
-#         return self.name
-
-
-# class CustomPermission(models.Model):
-#     action = models.CharField(max_length=255)
-#     role = models.ForeignKey(UserRole, on_delete=models.CASCADE, related_name='permissions')
-#
-#     def __str__(self):
-#         return f"{self.action} - {self.role}"
-
-
-class Address(models.Model):
+class Address(TimeStampedMixin, LogicalMixin):
     user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='CustomerAddress')
     name = models.CharField(max_length=255)
     detail_address = models.TextField(max_length=300)
@@ -53,4 +40,9 @@ class Address(models.Model):
     description = models.TextField(max_length=355)
     city = models.CharField(max_length=255)
     province = models.CharField(max_length=255)
-    is_deleted = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ['user', 'name']
+
+    def __str__(self):
+        return f'{self.name} - {self.city} - {self.province}'
