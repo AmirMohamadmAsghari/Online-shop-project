f1ed4b1383f577e119ca2db4773061c513d4ebca
Amir <amirm2.asghari0@gmail.com>
2024-04-27 20:12:27 +0330

    Updating order app models Add meta class to models

--- apps/order/models.py
+++ apps/order/models.py
@@ -2,37 +2,44 @@ from django.db import models
 from apps.user.models import CustomUser
 from apps.product.models import Product
 from apps.user.models import Address
+from apps.core.models import TimeStampedMixin, LogicalMixin
 # Create your models here
 
 
-class Order(models.Model):
+class Order(TimeStampedMixin, LogicalMixin):
     customer = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='CustomerOrder')
     total_amount = models.DecimalField(max_digits=10, decimal_places=2)
     address = models.ForeignKey(Address, on_delete=models.CASCADE, related_name='AddressesOrder')
-    order_date = models.DateTimeField(auto_now_add=True)
     discount_code = models.ForeignKey('CodeDiscount', on_delete=models.CASCADE, related_name='DiscountOrder', null=True, blank=True)
     status = models.BooleanField(default=False)
-    is_deleted = models.BooleanField(default=False)
+
+    class Meta:
+        verbose_name = "Order"
+        verbose_name_plural = "Orders"
+        ordering = ['-created']
+
+    def __str__(self):
+        return f"Order {self.id} by {self.customer}"
 
 
-class OrderItem(models.Model):
+class OrderItem(TimeStampedMixin, LogicalMixin):
     order = models.ForeignKey('Order', on_delete=models.CASCADE, related_name='OrderItem')
     product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='ProductOrder')
     quantity = models.IntegerField(default=0)
     total_price = models.DecimalField(max_digits=10, decimal_places=2)
-    is_deleted = models.BooleanField(default=False)
+
+    class Meta:
+        verbose_name = "OrderItem"
+        ordering = ["-created"]
 
 
-class CodeDiscount(models.Model):
+class CodeDiscount(LogicalMixin, TimeStampedMixin):
     amount = models.IntegerField(default=0)
     code = models.IntegerField()
-    is_active = models.BooleanField(default=False)
-    is_deleted = models.BooleanField(default=False)
 
 
-class Payment(models.Model):
+class Payment(TimeStampedMixin):
     order = models.ForeignKey('Order', on_delete=models.CASCADE, related_name='OrderPayment')
-    payment_date = models.DateTimeField(auto_now_add=True)
     payment_type = models.CharField(max_length=255)
     transaction_id = models.IntegerField()
 
