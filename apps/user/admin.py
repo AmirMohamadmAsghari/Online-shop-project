7a58ccb720510b81439639734052621238c4d0af
Amir <amirm2.asghari0@gmail.com>
2024-04-27 20:08:02 +0330

    user app: Making changes to fix some errors in the admin file

--- apps/user/admin.py
+++ apps/user/admin.py
@@ -1,35 +1,41 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import CustomPermission, CustomUser, UserRole
+from .models import CustomUser,  Address
 
 
 class CustomUserAdmin(UserAdmin):
-    list_display = ('email', 'username', 'phone', 'is_staff', 'is_active', 'date_joined')
+    list_display = ('email', 'username', 'phone', 'is_staff','date_joined')
     search_fields = ('email', 'username', 'phone')
-    list_filter = ('is_staff', 'is_active')
+    #list_filter = 'is_staff'
     fieldsets = (
         (None, {'fields': ('email', 'password')}),
         ('Personal Info', {'fields': ('username', 'phone', 'name')}),
-        ('Permissions', {'fields': ('is_staff', 'is_active', 'role', 'groups', 'user_permissions')}),
+        ('Permissions', {'fields': ('is_staff', 'groups', 'user_permissions')}),
     )
     add_fieldsets = (
         (None, {
             'classes': ('wide',),
-            'fields': ('email', 'password1', 'password2', 'username', 'phone', 'name', 'is_staff', 'is_active', 'role')}
+            'fields': ('email', 'password1', 'password2', 'username', 'phone', 'name', 'is_staff', 'is_active')}
          ),
     )
 
-    def role_name(self, obj):
-        return obj.role.name if obj.role else '-'
+    # def role_name(self, obj):
+    #     return obj.role.name if obj.role else '-'
 
 
 admin.site.register(CustomUser, CustomUserAdmin)
-admin.site.register(UserRole)
+# admin.site.register(UserRole)
 
 
-class PermissionAdmin(admin.ModelAdmin):
-    list_display = ('action', 'role')
-    search_fields = ('action', 'role__name')
+# class PermissionAdmin(admin.ModelAdmin):
+#     list_display = ('action', 'role')
+#     search_fields = ('action', 'role__name')
+#
 
+class AddressAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'name', 'postal_code', 'city', 'province')
+    search_fields = ('user__email', 'name', 'postal_code')
+    #list_filter = ('city', 'province')
 
-admin.site.register(CustomPermission, PermissionAdmin)
+admin.site.register(Address, AddressAdmin)
+# admin.site.register(CustomPermission, PermissionAdmin)
