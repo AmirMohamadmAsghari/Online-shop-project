d72ccaa419937197545b9bed51c376a50e6abca4
Amir <amirm2.asghari0@gmail.com>
2024-05-05 19:42:07 +0330

    Url for the user's email confirmation view after registering

--- apps/user/urls.py
+++ apps/user/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import RegisterUserView, LoginUserView, SendOTPCodeView, LogoutView
+from .views import RegisterUserView, LoginUserView, SendOTPCodeView, LogoutView, Email_Verification
 
 
 urlpatterns = [
     path('register/', RegisterUserView.as_view(), name='register' ),
+    path('emailverification/', Email_Verification.as_view(), name='email_verification'),
     path('login/', LoginUserView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
     path('send-otp/', SendOTPCodeView.as_view(), name='send_otp'),
