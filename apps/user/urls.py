7fc25d0a869af679e710cdf60acd88807ec9fe6d
Amir <amirm2.asghari0@gmail.com>
2024-05-27 22:02:46 +0330

    Create addresses, delete addresses, modify addresses, and select addresses

--- apps/user/urls.py
+++ apps/user/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import RegisterUserView, LoginUserView, SendOTPCodeView, LogoutView, Email_Verification
+from .views import RegisterUserView, LoginUserView, SendOTPCodeView, LogoutView, Email_Verification, AddressCreateView, AddressListView, AddressSelectView, UserProfileView, AddressEditView, AddressDeleteView
 from django.conf.urls import handler404
 from .views import custom_404_view
 
@@ -10,5 +10,12 @@ urlpatterns = [
     path('login/', LoginUserView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
     path('send-otp/', SendOTPCodeView.as_view(), name='send_otp'),
+    path('addresses', AddressListView.as_view(), name='address-list'),
+    path('address/create', AddressCreateView.as_view(), name='address-create'),
+    path('address/select', AddressSelectView.as_view(), name='address-select'),
+    path('address/<int:address_id>/edit', AddressEditView.as_view(), name='address-edit'),
+    path('address/<int:address_id>/delete', AddressDeleteView.as_view(), name='address-delete'),
+    path('profile/', UserProfileView.as_view(), name='profile'),
+
 ]
 handler404 = custom_404_view
