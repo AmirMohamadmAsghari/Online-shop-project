0744f2300e7234fa448939c0cea7664cb454bc73
Amir <amirm2.asghari0@gmail.com>
2024-05-27 22:09:43 +0330

    Class to create delete and modify address

--- apps/user/views.py
+++ apps/user/views.py
@@ -1,15 +1,19 @@
 import json
 from django.http import HttpResponse, JsonResponse
-from .models import CustomUser
-from django.shortcuts import render, redirect
+from .models import CustomUser, Address
+from apps.order.models import Order
+from django.shortcuts import render, redirect, get_object_or_404
 from django.core.exceptions import ValidationError, ObjectDoesNotExist
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib import messages
 from django.views import View
 from .validators import validate_password_strength
 from .utils import generate_otp, send_otp_email, store_otp_in_redis
 import redis
+from django.urls import reverse
 from django.core.validators import validate_email
+
 
 # Create your views here.
 
@@ -58,13 +62,13 @@ class RegisterUserView(View):
 class Email_Verification(View):
     template_name = 'email_verification.html'
 
-    def get(self,request):
+    def get(self, request):
         email = request.session.get('email')
         otp_code = generate_otp(email)
         send_otp_email(email, otp_code)
         return render(request, self.template_name)
 
-    def post(self,request):
+    def post(self, request):
         otp_code = request.POST.get('otp_code')
         email = request.session.get('email')
         try:
@@ -171,11 +175,95 @@ class LoginUserView(View):
 
 
 class LogoutView(View):
-    def get(self,request):
+    def get(self, request):
         logout(request)
         messages.success(request, 'Logged out successfully.')
         return redirect('home')
 
 
+def custom_404_view(request, exception):
+    return render(request, '404.html', status=404)
 
 
+class AddressCreateView(LoginRequiredMixin, View):
+    def get(self, request):
+        return render(request, 'create_address.html')
+
+    def post(self, request):
+        name = request.POST.get('name')
+        detail_address = request.POST.get('detail_address')
+        postal_code = request.POST.get('postal_code')
+        description = request.POST.get('description')
+        city = request.POST.get('city')
+        province = request.POST.get('province')
+
+        address = Address(
+            user=request.user,
+            name=name,
+            detail_address=detail_address,
+            postal_code=postal_code,
+            description=description,
+            city=city,
+            province=province
+        )
+        address.save()
+        messages.success(request, 'Address created successfully.')
+        return redirect('address-list')
+
+
+class AddressListView(LoginRequiredMixin, View):
+    def get(self, request):
+        addresses = Address.objects.filter(user=request.user)
+        return render(request, 'address_list.html', {'addresses': addresses})
+
+
+class AddressSelectView(LoginRequiredMixin, View):
+    def post(self, request):
+        address_id = request.POST.get('address_id')
+        try:
+            address = Address.objects.get(id=address_id, user=request.user)
+            order = Order.objects.get(customer=request.user, status='open')
+            order.address = address
+            order.save()
+            messages.success(request, 'Address selected successfully.')
+        except Address.DoesNotExist:
+            messages.error(request, 'Address not found')
+        except Order.DoesNotExist:
+            messages.error(request, 'Open Order Not found')
+        return redirect('view-order')
+
+
+class AddressEditView(LoginRequiredMixin, View):
+    def get(self, request, address_id):
+        address = get_object_or_404(Address, id=address_id, user=request.user)
+        return render(request, 'address_edit.html', {'address':address})
+
+    def post(self, request, address_id):
+        address = get_object_or_404(Address, id=address_id, user=request.user)
+        address.name = request.POST.get('name')
+        address.detail_address = request.POST.get('detail_address')
+        address.postal_code = request.POST.get('postal_code')
+        address.description = request.POST.get('description')
+        address.city = request.POST.get('city')
+        address.province = request.POST.get('province')
+        address.save()
+        messages.success(request, 'Address updated successfully')
+        return redirect('address-list')
+
+
+class AddressDeleteView(LoginRequiredMixin, View):
+    def post(self, request, address_id):
+        address = get_object_or_404(Address, id=address_id, user=request.user)
+        address.delete()
+        messages.success(request, 'Address deleted successfully.')
+        return redirect('address-list')
+
+
+
+
+
+class UserProfileView(LoginRequiredMixin, View):
+    def get(self, request):
+        addresses = Address.objects.filter(user=request.user)
+        orders = Order.objects.filter(customer=request.user)
+        return render(request, 'user_profile.html', {'addresses': addresses, 'orders': orders})
