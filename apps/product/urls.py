d0ffaed3da521d9a928e9d10ad43e5e4296f32eb
Amir <amirm2.asghari0@gmail.com>
2024-05-28 19:42:06 +0330

    Updated urls to better display categories products app

--- apps/product/urls.py
+++ apps/product/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
-from .views import ProductView, CategoryListAPIView, ProductListAPIView, ImageListAPIView, ProductDetailView, \
-    ReviewsAPIView
+from .views import ProductView, CategoryListAPIView, ProductListAPIView, ImageListAPIView, ProductDetailView, CategoryProductView, ReviewsAPIView
 from django.conf.urls import handler404
 from .views import custom_404_view
 
 urlpatterns = [
     path('products/', ProductView.as_view(), name='products'),
+    path('products/category/<int:category_id>/', CategoryProductView.as_view(), name='category-products'),
     path('productsapi/', ProductListAPIView.as_view(), name='productsapi'),
     path('categoriesapi/', CategoryListAPIView.as_view(), name='categoryapi'),
     path('image/', ImageListAPIView.as_view(), name='imageapi'),
