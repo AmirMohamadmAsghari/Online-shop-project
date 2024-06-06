fced8e4ec29dd52ac3944e007916771d2566bd25
Amir <amirm2.asghari0@gmail.com>
2024-06-06 21:10:55 +0330

    Update the manager core app

--- apps/core/managers.py
+++ apps/core/managers.py
@@ -3,29 +3,25 @@ from django.db import models
 
 class LogicalQuerySet(models.QuerySet):
     def undelete(self):
-        return super().update(is_deleted=False)
+        return self.update(is_deleted=False)
 
     def delete(self):
-        return super().update(is_deleted=True)
+        return self.update(is_deleted=True)
 
     def activate(self):
-        return super().update(is_active=True)
+        return self.update(is_active=True)
 
     def deactivate(self):
-        return super().update(is_active=False)
+        return self.update(is_active=False)
 
 
 class LogicalManager(models.Manager):
-    def get_queryset_object(self):
-        if not hasattr(self.__class__, "__queryset"):
-            self.__class__.__queryset = LogicalQuerySet(self.model)
-        return self.__queryset
-
     def get_queryset(self):
-        return super().get_queryset().filter(is_deleted=False)
+        return LogicalQuerySet(self.model, using=self._db).filter(is_deleted=False)
 
-    @property
-    def archived(self):
-        return super().get_queryset()
+    def all_with_deleted(self):
+        return LogicalQuerySet(self.model, using=self._db)
 
+    def deleted(self):
+        return LogicalQuerySet(self.model, using=self._db).filter(is_deleted=True)
 
