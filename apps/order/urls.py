c3d70c1e01c0f75811142996db975cafbd762e78
Amir <amirm2.asghari0@gmail.com>
2024-06-06 21:21:20 +0330

    Address to delete order item

--- apps/order/urls.py
+++ apps/order/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
-from .views import OrderItemCreate, OrderAPIView, AddressAPIView, AddToCartView, CheckOutView, OrderView, PaymentInitiateView, PaymentSuccessView
+from .views import OrderItemCreate, OrderAPIView, AddressAPIView, AddToCartView, CheckOutView, OrderView, PaymentInitiateView, PaymentSuccessView, OrderItemDeleteView
 
 urlpatterns = [
     path('orderitem', OrderItemCreate.as_view(), name='orderitem'),
     path('order',OrderAPIView.as_view(), name='order'),
     path('address', AddressAPIView.as_view(), name='address'),
     path('add-to-cart/', AddToCartView.as_view(), name='add-to-cart'),
+    path('api/order-item/<int:pk>/', OrderItemDeleteView.as_view(), name='order-item-delete'),
     path('view-order/', OrderView.as_view(), name='view-order'),
     path('checkout/', CheckOutView.as_view(), name='checkout'),
     path('payment/<int:order_id>/initiate', PaymentInitiateView.as_view(), name='payment-initiate'),
