5f411bfe3aa0bab9a1430b65a7d2ea6e084bb903
Amir <amirm2.asghari0@gmail.com>
2024-06-19 15:24:04 +0330

    Update urls User App

--- apps/user/urls.py
+++ apps/user/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
-from .views import RegisterUserView, LoginUserView, SendOTPCodeView, LogoutView, Email_Verification, AddressCreateView, AddressListView, AddressSelectView, UserProfileView, AddressEditView, AddressDeleteView
+from .views import RegisterUserView, LoginUserView, SendOTPCodeView, LogoutView, Email_Verification, AddressCreateView, \
+    AddressListView, AddressSelectView, UserProfileView, AddressEditView, AddressDeleteView
 from django.conf.urls import handler404
 from .views import custom_404_view
 
-
 urlpatterns = [
-    path('register/', RegisterUserView.as_view(), name='register' ),
+    path('register/', RegisterUserView.as_view(), name='register'),
     path('emailverification/', Email_Verification.as_view(), name='email_verification'),
     path('login/', LoginUserView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
