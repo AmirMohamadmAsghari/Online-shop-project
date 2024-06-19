ffa7685400573e71f94161c494e8624630d0abbe
Amir <amirm2.asghari0@gmail.com>
2024-06-19 15:16:11 +0330

    Def for update total amount in order model

--- apps/order/models.py
+++ apps/order/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from apps.user.models import CustomUser
 from apps.product.models import Product
-from apps.user.models import Address
+from apps.user.models import Address, CodeDiscount
 from apps.core.models import TimeStampedMixin, LogicalMixin
 # Create your models here
 
@@ -14,9 +14,29 @@ class Order(TimeStampedMixin, LogicalMixin):
         # Add other statuses as needed
     ]
     customer = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='CustomerOrder', null=True)
-    total_amount = models.DecimalField(max_digits=10, decimal_places=2, null=True)
+    total_amount = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
     address = models.ForeignKey(Address, on_delete=models.CASCADE, related_name='AddressesOrder', null=True)
     status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='open')
+    discount_code = models.CharField(max_length=255, null=True, blank=True)
+
+    def update_total_amount(self):
+        base_total = sum(item.quantity * item.price for item in self.item.all())
+
+        discount = None
+        if self.discount_code:
+            try:
+                discount = CodeDiscount.objects.get(code=self.discount_code)
+                if base_total >= discount.minimum_purchase_amount:
+                    if discount.type == 'percentage':
+                        discount_amount = (discount.amount / 100) * base_total
+                    elif discount.type == 'fixed':
+                        discount_amount = discount.amount
+                    base_total -= discount_amount
+            except CodeDiscount.DoesNotExist:
+                pass  # Invalid discount code, ignore it
+
+        self.total_amount = max(base_total, 0)  # Ensure the total amount is not negative
+        self.save()
 
     class Meta:
         verbose_name = "Order"
@@ -36,6 +56,7 @@ class OrderItem(TimeStampedMixin, LogicalMixin):
     quantity = models.IntegerField(default=0)
     total_price = models.DecimalField(max_digits=10, decimal_places=2)
 
+
     class Meta:
         verbose_name = "OrderItem"
         ordering = ["-created"]
@@ -46,5 +67,6 @@ class Payment(TimeStampedMixin):
     payment_type = models.CharField(max_length=255)
     transaction_id = models.IntegerField()
     amount = models.DecimalField(max_digits=10, decimal_places=2)
+    status = models.CharField(max_length=20, default='pending')
 
 
