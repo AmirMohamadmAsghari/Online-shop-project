b132d91de90a2e19c8963f63708e636bbefef07a
Amir <amirm2.asghari0@gmail.com>
2024-07-06 15:06:16 +0330

    Set Language Address

--- public/urls.py
+++ public/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
-from .views import Home_View
+from .views import Home_View, set_language
+
 urlpatterns = [
     path('', Home_View.as_view(), name='home'),
+    path('set_language/', set_language, name='set_language'),
 ]
