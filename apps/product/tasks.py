ab49d0444526cb822bc97dec5088f936b16b00df
Amir <amirm2.asghari0@gmail.com>
2024-07-07 14:57:01 +0330

    Remove expired discounts

---
+++ apps/product/tasks.py
@@ -0,0 +1,15 @@
+from celery import shared_task
+from django.utils import timezone
+from apps.product.models import Discount
+
+
+@shared_task
+def clear_expired_discounts():
+    now = timezone.now()
+    expired_discounts = Discount.objects.filter(expiration_date__lt=now)
+    print(f"Found {expired_discounts.count()} expired discounts.")
+
+    for discount in expired_discounts:
+        discount.is_deleted = True
+        discount.save()
+        print(f"Discount ID {discount.id}: is_delete set to {discount.is_deleted}")
