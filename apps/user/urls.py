7d1ae8ce1082f72303406e77405355ca9c76cb56
Amir <amirm2.asghari0@gmail.com>
2024-07-09 14:18:26 +0330

    Password forgotten path

--- apps/user/urls.py
+++ apps/user/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from .views import RegisterUserView, LoginUserView, SendOTPCodeView, LogoutView, Email_Verification, AddressCreateView, \
     AddressListView, AddressSelectView, UserProfileView, AddressEditView, AddressDeleteView
-from django.conf.urls import handler404
-from .views import custom_404_view
 
+from .views import custom_404_view
+from django.contrib.auth import views as auth_views
+from .views import CustomPasswordResetView
 urlpatterns = [
     path('register/', RegisterUserView.as_view(), name='register'),
     path('emailverification/', Email_Verification.as_view(), name='email_verification'),
@@ -16,6 +17,11 @@ urlpatterns = [
     path('address/<int:address_id>/edit', AddressEditView.as_view(), name='address-edit'),
     path('address/<int:address_id>/delete', AddressDeleteView.as_view(), name='address-delete'),
     path('profile/', UserProfileView.as_view(), name='profile'),
+    path('password_reset/', CustomPasswordResetView.as_view(), name='password_reset'),
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+
 
 ]
 handler404 = custom_404_view
