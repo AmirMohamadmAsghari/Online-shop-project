bb7639db0f261ecf06139d9eb1f077dd9ca762bd
Amir <amirm2.asghari0@gmail.com>
2024-07-11 23:53:07 +0330

    Remove the extra line

--- apps/order/models.py
+++ apps/order/models.py
@@ -11,7 +11,6 @@ class Order(TimeStampedMixin, LogicalMixin):
         ('open', 'Open'),
         ('closed', 'Closed'),
         ('pending', 'Pending'),
-        # Add other statuses as needed
     ]
     customer = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='CustomerOrder', null=True)
     total_amount = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
@@ -19,24 +18,24 @@ class Order(TimeStampedMixin, LogicalMixin):
     status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='open')
     discount_code = models.CharField(max_length=255, null=True, blank=True)
 
-    def update_total_amount(self):
-        base_total = sum(item.quantity * item.price for item in self.item.all())
-
-        discount = None
-        if self.discount_code:
-            try:
-                discount = CodeDiscount.objects.get(code=self.discount_code)
-                if base_total >= discount.minimum_purchase_amount:
-                    if discount.type == 'percentage':
-                        discount_amount = (discount.amount / 100) * base_total
-                    elif discount.type == 'fixed':
-                        discount_amount = discount.amount
-                    base_total -= discount_amount
-            except CodeDiscount.DoesNotExist:
-                pass  # Invalid discount code, ignore it
-
-        self.total_amount = max(base_total, 0)  # Ensure the total amount is not negative
-        self.save()
+    # def update_total_amount(self):
+    #     base_total = sum(item.quantity * item.price for item in self.item.all())
+    #
+    #     discount = None
+    #     if self.discount_code:
+    #         try:
+    #             discount = CodeDiscount.objects.get(code=self.discount_code)
+    #             if base_total >= discount.minimum_purchase_amount:
+    #                 if discount.type == 'percentage':
+    #                     discount_amount = (discount.amount / 100) * base_total
+    #                 elif discount.type == 'fixed':
+    #                     discount_amount = discount.amount
+    #                 base_total -= discount_amount
+    #         except CodeDiscount.DoesNotExist:
+    #             pass  # Invalid discount code, ignore it
+    #
+    #     self.total_amount = max(base_total, 0)  # Ensure the total amount is not negative
+    #     self.save()
 
     class Meta:
         verbose_name = "Order"
